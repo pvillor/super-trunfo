2fa0ef617c3e351577178e603a7cec1e5fc451d1
pvillor <pvillor@gmail.com>
2023-04-14 17:54:39 -0300

    feat: battle

--- pokemon/game.py
+++ pokemon/game.py
@@ -6,4 +6,18 @@ def play():
 
     p1_deck, p2_deck = split_deck(pokemon_deck)
 
-    random_attr = get_random_attr(p1_deck[0])
+    for index, p1_card in enumerate(p1_deck):
+        print()
+        p2_card = p2_deck[index]
+        attr_to_compare = get_random_attr(p1_deck[0])
+
+        print('p1_card -> ', p1_card)
+        print('p2_card -> ', p2_deck[index])
+        print('attr_to_compare -> ', attr_to_compare)
+
+        if p1_card[attr_to_compare] > p2_card[attr_to_compare]:
+            print(f"{p1_card['name']} WINS")
+        elif p2_card[attr_to_compare] > p1_card[attr_to_compare]:
+            print(f"{p2_card['name']} WINS")
+        else:
+            print('DRAW!')
